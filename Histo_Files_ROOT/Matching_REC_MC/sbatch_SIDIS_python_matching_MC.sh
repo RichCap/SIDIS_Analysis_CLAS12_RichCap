062f12f435df99bace509a6bcf8cd7c55cd5b135
Richard Capobianco <richcap@ifarm1802.jlab.org>
2024-05-10 12:55:12 -0400

    Basic update on 5-10-2024 (all major changes were made to other files)

--- Histo_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC.sh
+++ Histo_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=mdf_SIDIS_5_9_2024_Run1_5D_Unfold_Test_V3_SIDIS
+#SBATCH --job-name=NS_mdf_SIDIS_5_9_2024_Run1_5D_Unfold_Test_V3_SIDIS
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -15,4 +15,4 @@
 FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/MC_Matching_sidis_epip_richcap.inb.qa.45nA_job_*)
 # Above is for (mdf) #SBATCH --array=0-219
 
-srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_sidis ${FILES[$SLURM_ARRAY_TASK_ID]}
+srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_sidis_NS ${FILES[$SLURM_ARRAY_TASK_ID]}
--- sbatch_SIDIS_unfold_python.sh
+++ sbatch_SIDIS_unfold_python.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=P2_RooUnfold_New_Q2_Y_Bins_V5_4_16_2024_Run1
+#SBATCH --job-name=P2_RooUnfold_5D_Unfold_Test_V1_5_8_2024_Run1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
 #SBATCH --error=/farm_out/%u/%x-%A_%a-%j-%N.err
 #SBATCH --partition=production
 #SBATCH --account=clas12
-#SBATCH --mem-per-cpu=13000
+#SBATCH --mem-per-cpu=9000
 #SBATCH --time=0:40:00
-#SBATCH --array=0-17
+#SBATCH --array=0,3,6-14,16-17
 
 
 # source /group/clas12/packages/setup.csh
 # source /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/New_RooUnfold/RooUnfold/build/setup.sh
 # Main array: SBATCH --array=0-17
+# Normal request: #SBATCH --mem-per-cpu=13000
 
-srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/RooUnfold_SIDIS_richcap.py smear $SLURM_ARRAY_TASK_ID
+srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/RooUnfold_SIDIS_richcap.py smear_no_txt $SLURM_ARRAY_TASK_ID
