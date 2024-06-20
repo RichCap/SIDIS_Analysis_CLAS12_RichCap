f1aa5270fe06b5b6c510258fd95fad272b045ec3
Richard Capobianco <richcap@ifarm1802.jlab.org>
2024-06-20 14:01:04 -0400

    Ran Momentum Smearing version of code (with new cuts)

--- Histo_Files_ROOT/Matching_REC_MC/sbatch_Mom_python_matching_MC_Pass2.sh
+++ Histo_Files_ROOT/Matching_REC_MC/sbatch_Mom_python_matching_MC_Pass2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=P2_mdf_Mom_5_2_2024_Run1_Pass_2_New_Smear_V9_Mom
+#SBATCH --job-name=P2_mdf_Mom_6_14_2024_Run2_Pass_2_New_Smear_V13_Mom
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -10,9 +10,14 @@
 #SBATCH --mem-per-cpu=3500
 #SBATCH --time=4:00:00
 #SBATCH --array=0-53
+#SBATCH --constraint=el7
+
+# FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/With_BeamCharge/Pass2/MC_Matching_sidis_epip_richcap.inb.qa.inb-clasdis_*)
+## Above is for (mdf) #SBATCH --array=0-53
+
+FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/With_BeamCharge/Pass2/More_Cut_Info/MC_Matching_sidis_epip_richcap.inb.qa.new2.inb-clasdis_*)
+# Above is for (mdf_NewP2) #SBATCH --array=0-53
+# Normally requested time: --time=14:00:00
 
 
-FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/With_BeamCharge/Pass2/MC_Matching_sidis_epip_richcap.inb.qa.inb-clasdis_*)
-# Above is for (mdf) #SBATCH --array=0-53
-
-srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_mom_P2 ${FILES[$SLURM_ARRAY_TASK_ID]}
+srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_mom_NewP2 ${FILES[$SLURM_ARRAY_TASK_ID]}
--- Histo_Files_ROOT/REAL_Data/sbatch_Mom_python_real_data_Pass2.sh
+++ Histo_Files_ROOT/REAL_Data/sbatch_Mom_python_real_data_Pass2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=P2_rdf_Mom_4_17_2024_Run1_Pass_2_New_Smear_V5_Mom
+#SBATCH --job-name=P2_rdf_Mom_6_14_2024_Run2_Pass_2_New_Smear_V13_Mom
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -10,9 +10,14 @@
 #SBATCH --mem-per-cpu=2500
 #SBATCH --time=6:00:00
 #SBATCH --array=0-170
+#SBATCH --constraint=el7
+
+# FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/REAL_Data/Pass2/Data_sidis_epip_richcap.inb.qa.nSidis_005*)
+# # Above is for (rdf) #SBATCH --array=0-170
+
+FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/REAL_Data/Pass2/More_Cut_Info/Data_sidis_epip_richcap.inb.qa.new2.nSidis_005*)
+# Above is for (rdf_NewP2) #SBATCH --array=0-170
+# Normally requested time: --time=8:00:00
 
 
-FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/REAL_Data/Pass2/Data_sidis_epip_richcap.inb.qa.nSidis_005*)
-# Above is for (rdf) #SBATCH --array=0-170
-
-srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py rdf_mom_P2 ${FILES[$SLURM_ARRAY_TASK_ID]}
+srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py rdf_mom_NewP2 ${FILES[$SLURM_ARRAY_TASK_ID]}
