3708eb56fa02e1cb4306ffcd65487641dba601ae
richcap <richard.capobianco@uconn.edu>
2024-07-29 18:46:29 -0400

    Needed to rerun

--- Data_Files_Groovy/GEN_MC/SIDIS_MC_GEN_epip_TTree_Batch_Pass2_My_New_UpToDate.sh
+++ Data_Files_Groovy/GEN_MC/SIDIS_MC_GEN_epip_TTree_Batch_Pass2_My_New_UpToDate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=My_GEN_sidis_epip_TTree_7_29_2024_P2_WithPionCount_R1
+#SBATCH --job-name=My_GEN_sidis_epip_TTree_7_29_2024_P2_WithPionCount_R2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -9,7 +9,7 @@
 #SBATCH --account=clas12
 #SBATCH --mem-per-cpu=5500
 #SBATCH --time=4:00:00
-#SBATCH --array=0-9
+#SBATCH --array=9
 
 
 FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/New_MC_hipo/BeamEnergy_1060/inb-clasdis*)
