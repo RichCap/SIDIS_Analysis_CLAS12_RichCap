6dfb1fbd853ca40ed4811e7bf457c2364f70200c
richcap <richard.capobianco@uconn.edu>
2024-07-29 18:06:06 -0400

    Fixed array range

--- Data_Files_Groovy/GEN_MC/sbatch_SIDIS_MC_GEN_epip_Proton_TTree_Batch_Pass2_UpToDate.sh
+++ Data_Files_Groovy/GEN_MC/sbatch_SIDIS_MC_GEN_epip_Proton_TTree_Batch_Pass2_UpToDate.sh
@@ -9,10 +9,10 @@
 #SBATCH --account=clas12
 #SBATCH --mem-per-cpu=5500
 #SBATCH --time=4:00:00
-#SBATCH --array=0-54
+#SBATCH --array=0-53
 
 
 FILES=(/lustre19/expphy/volatile/clas12/sdiehl/osg_out/clasdis/inb-clasdis_*)
-# Above is for #SBATCH --array=0-54
+# Above is for #SBATCH --array=0-53
 
 srun /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_Proton_Batch_Pass2_UpToDate.groovy ${FILES[$SLURM_ARRAY_TASK_ID]}
