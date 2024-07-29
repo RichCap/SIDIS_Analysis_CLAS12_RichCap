1dcbfcb7570068823d738f626f490a898c4f0b74
richcap <richard.capobianco@uconn.edu>
2024-07-29 15:19:15 -0400

    Updated MC GEN groovy scripts for file versions "new5" (Also added tagged proton script)

--- Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_Proton_Batch_Pass2_UpToDate.groovy
+++ Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_Proton_Batch_Pass2_UpToDate.groovy
@@ -26,13 +26,10 @@ else suff += '.qa'
 
 def outname = args[0].split("/")[-1]
 
-def ff = new ROOTFile("MC_Gen_sidis_epip_richcap.${suff}.new5.${outname}.root")
+def ff = new ROOTFile("MC_Gen_sidis_epip_richcap.${suff}.wProton.new5.${outname}.root")
 
-// // Added 'beamCharge' as of 1/29/2024
-// def tt = ff.makeTree('h22','title','event/I:runN/I:ex:ey:ez:pipx:pipy:pipz:esec/I:pipsec/I:Hx:Hy:beamCharge')
-
-// Added 'Num_Pions' as of 7/29/2024 (as part of version 'new5' - name used to match the reconstructed files)
-def tt = ff.makeTree('h22', 'title', 'event/I:runN/I:beamCharge:Num_Pions/I:ex:ey:ez:pipx:pipy:pipz:esec/I:pipsec/I:Hx:Hy')
+// Added 'Num_Pions' and Proton info as of 7/29/2024 (as part of version 'wProton.new5' - name used to match the reconstructed files)
+def tt = ff.makeTree('h22', 'title', 'event/I:runN/I:beamCharge:Num_Pions/I:ex:ey:ez:pipx:pipy:pipz:prox:proy:proz:esec/I:pipsec/I:Hx:Hy')
 
 int Multiple_Pions_Per_Electron = 0
 int Total_Events_Found = 0
@@ -64,6 +61,29 @@ args.eachParallel{fname->
             if(pid == 11){ // Is an electron
                 
                 int pionCount = 0 // Counter for pions (helps control double-counted electrons)
+
+                // Check for the presence of a proton (for Harut's Vector Meson Cuts)
+                boolean hasProton = false
+                // def proV = null // Declare proV to store the proton Lorentz vector
+                def prox = null
+                def proy = null
+                def proz = null
+                for (int ipart_proton = 1; ipart_proton < partb.getRows(); ipart_proton++) {
+                    def pid_pro = partb.getInt("pid", ipart)
+                    if(pid_pro == 2212){
+                        hasProton = true
+                        def prox = partb.getFloat("px", ipart_proton)
+                        def proy = partb.getFloat("py", ipart_proton)
+                        def proz = partb.getFloat("pz", ipart_proton)
+                        // def proV = LorentzVector.withPID(pid_pro, prox, proy, proz) // Get the Lorentz vector of the proton
+                        // Lorentz vector for proton is not in use due to not keeping the proton sector info
+                        break
+                    }
+                }
+                // Skip to the next event if no proton is found
+                if(!hasProton){
+                    continue
+                }
                 
                 for(int ipart = 1; ipart < partb.getRows(); ipart++){
                     def pid_pip = partb.getInt("pid", ipart)
@@ -90,16 +110,9 @@ args.eachParallel{fname->
                         float Hx = ecb.getFloat("hx", 0)
                         float Hy = ecb.getFloat("hy", 0)
 
-                        // // Spherical Momentum Coordinates
-                        // def el = ele.p()
-                        // def elth = (180/3.1415926)*ele.theta()
+                        // Spherical Momentum Coordinates
                         def elPhi = (180/3.1415926)*ele.phi()
-                        // def pip = pip0.p()
-                        // def pipth = (180/3.1415926)*pip0.theta()
                         def pipPhi = (180/3.1415926)*pip0.phi()
-
-                        // // Particle Energy
-                        // def el_E = ele.e(), pip_E = pip0.e()
 
                         // Electron Sectors (From Angle)
                         def esec_a = 0
@@ -119,12 +132,8 @@ args.eachParallel{fname->
                         if(pipPhi >= -105 && pipPhi <  -45){pipsec_a = 5}
                         if(pipPhi >= -165 && pipPhi < -105){pipsec_a = 6}
                         
-                        // tt.fill(evn, run, ex, ey, ez, px, py, pz,
-                        //     esec_a, pipsec_a, Hx, Hy, elec_events_found)
-                        // tt.fill(evn, run, ex, ey, ez, px, py, pz,
-                        //     esec_a, pipsec_a, Hx, Hy, beamCharge)
                         tt.fill(evn, run, beamCharge, pionCount,
-                                ex,  ey,  ez, px, py, pz,
+                                ex,  ey,  ez, px, py, pz,  prox, proy, proz
                             esec_a, pipsec_a, Hx, Hy)
                     }
                 }
