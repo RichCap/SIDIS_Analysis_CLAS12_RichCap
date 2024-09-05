a5c083e8b93e9a5e9dcb314dd8fbee9af529b70e
richcap <richard.capobianco@uconn.edu>
2024-09-05 17:35:53 -0400

    Running 'New_Fiducial_Cut_Test_FC_11_V10'

--- Histo_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC_Pass2.sh
+++ Histo_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC_Pass2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=NS_mdf_SIDIS_9_4_2024_Run1_New_Fiducial_Cut_Test_V10_SIDIS
+#SBATCH --job-name=FC_11_NS_mdf_SIDIS_9_5_2024_Run1_New_Fiducial_Cut_Test_V10_SIDIS
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -24,4 +24,4 @@ FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/With_BeamCharge/
 # Above is for (mdf_NewP2 - As of 8/22/2024) #SBATCH --array=0-113
 # Normally requested time: --time=14:00:00
 
-srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_sidis_NewP2_NS ${FILES[$SLURM_ARRAY_TASK_ID]}
+srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/makeROOT_epip_SIDIS_histos_new.py mdf_sidis_NewP2_NS_FC_11 ${FILES[$SLURM_ARRAY_TASK_ID]}
