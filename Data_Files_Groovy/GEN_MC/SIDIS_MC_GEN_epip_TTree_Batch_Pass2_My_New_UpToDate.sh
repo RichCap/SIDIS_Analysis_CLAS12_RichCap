cc15679bee3f043dd0368bc828fff357a8adad6d
richcap <richard.capobianco@uconn.edu>
2024-09-24 14:55:19 -0400

    Update of sbatch scripts

--- Data_Files_Groovy/GEN_MC/SIDIS_MC_GEN_epip_TTree_Batch_Pass2_My_New_UpToDate.sh
+++ Data_Files_Groovy/GEN_MC/SIDIS_MC_GEN_epip_TTree_Batch_Pass2_My_New_UpToDate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=My_GEN_sidis_epip_TTree_9_20_2024_P2_WithPionCount_R1
+#SBATCH --job-name=My_GEN_sidis_epip_TTree_9_21_2024_P2_WithPionCount_R2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -9,7 +9,7 @@
 #SBATCH --account=clas12
 #SBATCH --mem-per-cpu=5500
 #SBATCH --time=4:00:00
-#SBATCH --array=0-49
+#SBATCH --array=0-89
 
 
 # FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/New_MC_hipo/BeamEnergy_1060/inb-clasdis-7975*)
@@ -22,7 +22,11 @@ FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-
     /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8198_*
     /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8182_*
     /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8205_*
-    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8207_*)
-# Above is for #SBATCH --array=0-49
+    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8207_*
+    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8210_*
+    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8200_*
+    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8222_*
+    /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8199_*)
+# Above is for #SBATCH --array=0-89
 
 srun /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_Batch_Pass2_UpToDate.groovy ${FILES[$SLURM_ARRAY_TASK_ID]}
--- TTree_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC_Pass2.sh
+++ TTree_Files_ROOT/Matching_REC_MC/sbatch_SIDIS_python_matching_MC_Pass2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=mdf_TTree_SIDIS_9_13_2024_Run1_New_TTree_V1_SIDIS
+#SBATCH --job-name=mdf_TTree_SIDIS_9_21_2024_Run2_New_TTree_V1_SIDIS
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -9,11 +9,11 @@
 #SBATCH --account=clas12
 #SBATCH --mem-per-cpu=5000
 #SBATCH --time=14:00:00
-#SBATCH --array=0-133
+#SBATCH --array=0-223
 
 
 FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/With_BeamCharge/Pass2/More_Cut_Info/MC_Matching_sidis_epip_richcap.inb.qa.new5.inb-clasdis*)
-# Above is for (mdf_NewP2 - As of 9/13/2024) #SBATCH --array=0-133
+# Above is for (mdf_NewP2 - As of 9/21/2024) #SBATCH --array=0-223
 # Normally requested time: --time=14:00:00
 
 srun python3 /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/TTree_create_ROOT_epip_SIDIS.py mdf_sidis ${FILES[$SLURM_ARRAY_TASK_ID]}
