0fd35dc884ad3bb27cd869000b533e00676f059a
richcap <richard.capobianco@uconn.edu>
2024-11-08 13:51:44 -0500

    Failed rerun of the 'gdf' 'Fiducial_Cut_Test_V14' files

--- Histo_Files_ROOT/GEN_MC/sbatch_SIDIS_python_gen_MC_Pass2.sh
+++ Histo_Files_ROOT/GEN_MC/sbatch_SIDIS_python_gen_MC_Pass2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=P2_gdf_SIDIS_11_6_2024_Run1_New_Fiducial_Cut_Test_V14_SIDIS
+#SBATCH --job-name=P2_gdf_SIDIS_11_7_2024_Run2_New_Fiducial_Cut_Test_V14_SIDIS
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
 #SBATCH --error=/farm_out/%u/%x-%A_%a-%j-%N.err
 #SBATCH --partition=production
 #SBATCH --account=clas12
-#SBATCH --mem-per-cpu=2500
-#SBATCH --time=5:00:00
+#SBATCH --mem-per-cpu=5500
+#SBATCH --time=7:00:00
 #SBATCH --array=0-273
 
 
