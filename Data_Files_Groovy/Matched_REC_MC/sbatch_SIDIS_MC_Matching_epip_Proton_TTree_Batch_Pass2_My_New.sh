797b69ad830d2fc8347e14cb3aadb51c24a2b379
richcap <richard.capobianco@uconn.edu>
2025-05-09 14:54:54 -0400

    Replaced link 'sbatch_SIDIS_MC_Matching_epip_Proton_TTree_Batch_Pass2_My_New_UpToDate.sh' with the actual file 'sbatch_SIDIS_MC_Matching_epip_Proton_TTree_Batch_Pass2_My_New.sh'

---
+++ Data_Files_Groovy/Matched_REC_MC/sbatch_SIDIS_MC_Matching_epip_Proton_TTree_Batch_Pass2_My_New.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --job-name=P_MyMC_REC_SIDIS_epip_Proton_TTree_4_22_2024_Pass2_New_V5
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=richard.capobianco@uconn.edu 
+#SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%A_%a-%j-%N.err
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH --mem-per-cpu=5500
+#SBATCH --time=24:00:00
+#SBATCH --array=0-139
+
+
+# FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/New_MC_hipo/BeamEnergy_1060/inb-clasdis-7975*)
+# # Above is for #SBATCH --array=0-9
+
+FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8*)
+# Above is for #SBATCH --array=0-139
+
+
+srun /w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/MC_Matched_TTree_epip_Proton_Batch.groovy ${FILES[$SLURM_ARRAY_TASK_ID]}
