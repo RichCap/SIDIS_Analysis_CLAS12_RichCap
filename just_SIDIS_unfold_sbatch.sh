099818262eb4cfe5d9a27ee88c13a7ad425a27d1
richcap <richard.capobianco@uconn.edu>
2025-07-14 19:59:34 -0400

    Added new sbatch file to run with 'Just_RooUnfold_SIDIS_richcap.py'

---
+++ just_SIDIS_unfold_sbatch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --job-name=Just_RooUnfold_Sector_Integrated_Tests_V2_R1_eSectors_7_14_2025
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=richard.capobianco@uconn.edu 
+#SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%A_%a-%j-%N.err
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH --mem-per-cpu=15000
+#SBATCH --time=1:40:00
+#SBATCH --array=1-17
+
+
+# source /group/clas12/packages/setup.csh
+# source /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/New_RooUnfold/RooUnfold/build/setup.sh
+# Main array: SBATCH --array=0-17
+# Normal request: #SBATCH --mem-per-cpu=13000
+# no_txt request: #SBATCH --mem-per-cpu=9000
+
+srun /w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/./Just_RooUnfold_SIDIS_richcap.py -nf -smear $SLURM_ARRAY_TASK_ID
