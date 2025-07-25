9d460298d21b9852300a574c647b7a0f26452398
richcap <richard.capobianco@uconn.edu>
2025-07-25 12:51:37 -0400

    Ran EvGen files

--- Data_Files_Groovy/Matched_REC_MC/sbatch_SIDIS_MC_Matching_epip_TTree_Batch_Pass2_My_New.sh
+++ Data_Files_Groovy/Matched_REC_MC/sbatch_SIDIS_MC_Matching_epip_TTree_Batch_Pass2_My_New.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --job-name=MyMC_REC_SIDIS_epip_TTree_5_9_2025_Pass2_New_V5_R1
+#SBATCH --job-name=EvGen_REC_SIDIS_epip_TTree_7_16_2025_Pass2_New_V5_R1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=richard.capobianco@uconn.edu 
 #SBATCH --output=/farm_out/%u/%x-%A_%a-%j-%N.out
@@ -9,7 +9,7 @@
 #SBATCH --account=clas12
 #SBATCH --mem-per-cpu=5500
 #SBATCH --time=24:00:00
-#SBATCH --array=0-59
+#SBATCH --array=0-9
 
 
 # FILES=(/w/hallb-scshelf2102/clas12/richcap/SIDIS/New_MC_hipo/BeamEnergy_1060/inb-clasdis-7975*)
@@ -31,12 +31,15 @@
 # FILES=(/lustre24/expphy/volatile/clas12/akripko/inb-clasdis-osg_merged/inb-clasdis-*)
 # # Above is for #SBATCH --array=0-8
 
-FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8989_*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9040_*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8976_*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9030_*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9034_*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9047_*)
-# Above is for #SBATCH --array=0-59
+# FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8989_*
+#        /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9040_*
+#        /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-8976_*
+#        /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9030_*
+#        /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9034_*
+#        /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-clasdis-9047_*)
+# # Above is for #SBATCH --array=0-59
+
+FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-EvGen-LUND_EvGen_richcap_GEMC_Test-9427_*)
+# Above is for #SBATCH --array=0-9
 
 srun /w/hallb-scshelf2102/clas12/richcap/SIDIS/Matched_REC_MC/MC_Matched_TTree_epip_Batch.groovy ${FILES[$SLURM_ARRAY_TASK_ID]}
