c13896d81b7d7c18d850a02790264c156cd1889d
richcap <richard.capobianco@uconn.edu>
2025-10-21 14:31:37 -0400

    Adding more EvGen files

--- Data_Files_Groovy/GEN_MC/run_SIDIS_MC_GEN_EvGen_locally_with_email.sh
+++ Data_Files_Groovy/GEN_MC/run_SIDIS_MC_GEN_EvGen_locally_with_email.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-EvGen-LUND_EvGen_richcap_GEMC-9635*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-EvGen-LUND_EvGen_richcap_GEMC-9641*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-EvGen-LUND_EvGen_richcap_GEMC-9672*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/inb-EvGen-LUND_EvGen_richcap_GEMC-9674*)
+FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0002*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0003*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0009*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0012*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0017*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0065*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-0066*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9710*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9728*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9729*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9769*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9770*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9780*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/EvGen_Files_Not_Added_To_cache_yet/inb-EvGen-LUND_EvGen_richcap_GEMC-9781*)
 
 # Path to your Groovy script
 SCRIPT_PATH="/w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_EvGen.groovy"
@@ -20,7 +30,7 @@ for FILE in "${FILES[@]}"; do
 done
 
 
-JOB_ID="MC_Gen_epip_EvGen_10_2_2025"
+JOB_ID="MC_Gen_epip_EvGen_10_21_2025"
 
 # After processing all files, send a completion email
 EMAIL="richard.capobianco@uconn.edu"
