4f912fcf4a3d0e2f44310389a7b10cba69337d2e
richcap <richard.capobianco@uconn.edu>
2025-12-09 13:12:23 -0500

    Added more clasdis files to MC

--- Data_Files_Groovy/GEN_MC/run_SIDIS_MC_GEN_clasdis_locally_with_email.sh
+++ Data_Files_Groovy/GEN_MC/run_SIDIS_MC_GEN_clasdis_locally_with_email.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9682*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9704*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9707*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9708*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9711*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9714*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9719*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9739*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9740*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9742*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9747*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9752*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9753*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9754*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9755*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9756*
-       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9757*)
+FILES=(/lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9767*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9772*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9777*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9778*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9788*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9793*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9809*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9776*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9779*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9789*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/nb-clasdis-Q2_1.5-9795*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9884*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9930*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9972*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9885*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9886*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9964*
+       /lustre24/expphy/volatile/clas12/richcap/New_MC_SIDIS_Files_Volatile/clasdis_1.5_Cut_Files_Not_Added_To_cache_yet/nb-clasdis-Q2_1.5-9973*)
 
 # Path to your Groovy script
 SCRIPT_PATH="/w/hallb-scshelf2102/clas12/richcap/SIDIS_Analysis/Data_Files_Groovy/GEN_MC/MC_Gen_TTree_epip_Batch_Pass2_UpToDate.groovy"
@@ -34,7 +35,7 @@ for FILE in "${FILES[@]}"; do
 done
 
 
-JOB_ID="MC_Gen_epip_clasdis_10_21_2025"
+JOB_ID="MC_Gen_epip_clasdis_12_9_2025"
 
 # After processing all files, send a completion email
 EMAIL="richard.capobianco@uconn.edu"
